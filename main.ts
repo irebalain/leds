de00e54c6b42f3dd1fd2355a8cb94bc4a202e773
irebalain <57016660+irebalain@users.noreply.github.com>
2023-02-17 19:47:46 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(500)
+    }
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+})
